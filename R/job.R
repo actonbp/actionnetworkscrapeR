b9a0adb72b0cbcd6b174a749f85fe655677d2377
Bryan Acton <40258526+actonbp@users.noreply.github.com>
2022-01-29 17:18:35 -0500

    initial commit--lets hope actions works!

---
+++ R/job.R
@@ -0,0 +1,13 @@
+library(rvest)
+library(magrittr)
+url <- "https://www.actionnetwork.com/nba/public-betting"
+webpage <- read_html(url)
+
+x <- webpage %>% html_nodes(xpath = '/html/body/div[1]/div/main/div/div[3]/div[1]/div[2]/table')%>%
+  html_table()
+
+output <- x[1]
+
+setwd(here("data-raw"))
+
+write.csv(output, paste0(format(Sys.time(), "%d-%b-%Y %H.%M"), ".csv"))
